b8a57a03b0bf27f49f678301735a15582369bd9f
DarkProvider <53404124+DarkProvider@users.noreply.github.com>
2020-12-07 01:18:53 +0100

    Optimized the code more and added an inventory function as well as an item collecting system.

--- Space Disasters/Space Disasters/Program.cs
+++ Space Disasters/Space Disasters/Program.cs
@@ -20,7 +20,7 @@ namespace Space_Disasters
 	class Program
 	{
 
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
 			//Prints out the game title and overview, then it proceeds to the next sections
 			GameTitle();
@@ -30,7 +30,7 @@ namespace Space_Disasters
 
 
 		//====================================================================================//
-		//Identifiers: 
+		//Identifiers and classes:
 
 		//This will change the Console.writeline into Writeline instead
 		private static void Writeline(string text, Color color)
@@ -49,9 +49,13 @@ namespace Space_Disasters
 		//Character name identifier
 		private static String CharacterName;
 
+		//Inventory identifier
+		private static List<string> Inventory = new List<string>();
+
+		//This will read the input as a string
 		private static string input;
 
-
+		//This will change the color of the input and accept lowercase characters
 		private static void AnswerInput() 
 		{
 
@@ -61,6 +65,7 @@ namespace Space_Disasters
 			Console.ResetColor();
 		}
 
+		//This is just showing the "Please enter your choice: " text in Red
 		private static void EnterYourChoice() 
 		{
 
@@ -68,6 +73,7 @@ namespace Space_Disasters
 
 		}
 
+		//This is just showing the "Press any key to continue " text in Red
 		private static void PressEnter() 
 		{
 
@@ -76,6 +82,7 @@ namespace Space_Disasters
 
 		}
 
+		//This will display a thanks message
 		private static void ThanksMyDude() 
 		{
 
@@ -83,10 +90,7 @@ namespace Space_Disasters
 
 		}
 
-
-
-
-	//=========================================================================================//
+		//=========================================================================================//
 
 
 
@@ -102,7 +106,7 @@ namespace Space_Disasters
 
 
 
-		static void GameTitle()
+		private static void GameTitle()
 		{
 			//Call the maximizing funtion which has been added
 			Maximize();
@@ -148,7 +152,7 @@ namespace Space_Disasters
 
 
 		//Here is the game start code which calls for character and carrys on
-		static void StartGame()
+		private static void StartGame()
 		{
 				
 				Console.Write("Welcome ", Color.Blue);
@@ -170,7 +174,7 @@ namespace Space_Disasters
 
 
 		//Copyrights section
-		static void Copyrights()
+		private static void Copyrights()
 		{
 			//Sets the color to red and prints out the owner's name which is me, basically..
 			string owner = "Owner: Yazan Al Akkad\n";
@@ -195,7 +199,7 @@ namespace Space_Disasters
 
 
 		//asks player for name and save it
-		static void NameCharacter()
+		private static void NameCharacter()
 		{
 
 				Writeline("What would you like your name to be?\n", Color.Blue);
@@ -204,7 +208,7 @@ namespace Space_Disasters
 				Console.ResetColor();
 
 				CharacterName = Console.ReadLine();
-			Writeline("", Color.White);
+				Writeline("", Color.White);
 
 
 				Console.Write("Great! Nice to have you on board ", Color.Blue);
@@ -234,7 +238,7 @@ namespace Space_Disasters
 
 
 
-		static void Choice()
+		private static void Choice()
 		{
 			//Scenarios count and loop funtion
 			for (int scenario = 1; scenario <= Scenarios; scenario++)
@@ -249,52 +253,56 @@ namespace Space_Disasters
 						Console.WriteLine(PartOne[0] + PartOne[1] + PartOne[2] + PartOne[3] + PartOne[4] + PartOne[5], Color.Yellow);
 
 							EnterYourChoice();
-						AnswerInput();
+							AnswerInput();
 
 						if (input == "yes")
-							{
+							{							
 							Writeline(PartOne[6], Color.Green);
+							Inventory.Add("Batteries");
 							}
 						else
 							Writeline(PartOne[7], Color.Red);
 							
 						
 						Console.WriteLine(PartOne[8] + PartOne[9], Color.Yellow );
+						Inventory.Add("Cabinet Key" + "Hammer");
 
 							EnterYourChoice();
-						AnswerInput();
+							AnswerInput();
 
 						if (input == "yes")
 						{
 							Console.WriteLine(PartOne[10] + PartOne[11], Color.Yellow);
+							Inventory.Add("Crowbar");
 						}
 						else
 						{
+							Console.WriteLine(PartOne[12], Color.Red);						
+								Console.ReadKey();
 
-							Console.WriteLine(PartOne[12], Color.Red);
-							Console.ReadKey();
-							EndGame();
-							
+								Inventory.Clear();
+							EndGame();	
 						}
 
 						
 						Writeline("", Color.White);
-						PressEnter();
-						Console.ReadKey();
+							PressEnter();
+							Console.ReadKey();
 						
 						break;
 					case 2:
 						Console.Clear();
 
 						Writeline(PartTwo[0] + PartTwo[1], Color.Yellow);
+						Inventory.Add("Batteries");
 
 							EnterYourChoice();
-
-						AnswerInput();
+							AnswerInput();
 
 						if (input == "yes")
 						{
 							Writeline(PartTwo[3], Color.Yellow);
+							Inventory.Add("Door key" + "Flashlight withour batteries");
 						}
 						else
 						{
@@ -304,8 +312,8 @@ namespace Space_Disasters
 						Writeline(PartTwo[4], Color.Green);
 
 						Writeline("", Color.White);
-						PressEnter();
-						Console.ReadKey();
+							PressEnter();
+							Console.ReadKey();
 
 						break;
 					case 3:
@@ -315,8 +323,8 @@ namespace Space_Disasters
 						Writeline(PartThree[0], Color.Yellow);
 
 
-						EnterYourChoice();
-						AnswerInput();
+							EnterYourChoice();
+							AnswerInput();
 
 						if (input == "yes")
 						{
@@ -335,8 +343,12 @@ namespace Space_Disasters
 							{
 
 								Writeline(PartThree[5], Color.Red);
-								PressEnter();
-								Console.ReadKey();
+									PressEnter();
+									Console.ReadKey();
+								
+								Inventory.Clear();
+
+								//Calling the endgame class
 								EndGame();
 
 							}
@@ -345,13 +357,15 @@ namespace Space_Disasters
 							Writeline(PartThree[2], Color.Yellow);
 							
 						Writeline(PartThree[3], Color.Yellow);
+						Inventory.Add("Spare Parts");
 
 							EnterYourChoice();
-						AnswerInput();
+							AnswerInput();
 
 						if (input == "yes")
 						{
 							Writeline(PartThree[7], Color.Yellow);
+							Inventory.Add("Spacecraft Keys");
 						}
 						else
 							Writeline(PartThree[9], Color.Red);
@@ -360,12 +374,13 @@ namespace Space_Disasters
 						Writeline(PartThree[6], Color.Yellow);
 
 							EnterYourChoice();
-						AnswerInput();
+							AnswerInput();
 
 						if (input == "yes")
 						{
 
-							Writeline(PartThree[7], Color.Yellow);
+							Writeline(PartThree[7], Color.Yellow);							
+							Inventory.Add("Spacecraft Keys");
 
 							EnterYourChoice();
 							AnswerInput();
@@ -376,14 +391,17 @@ namespace Space_Disasters
 
 								Writeline(PartThree[11] + PartThree[12], Color.Yellow);
 
-								PressEnter();
-								Console.ReadKey();
+								Inventory.Add("Spacecraft Keys" + "Remainings of iron, copper and silicon" + "Astroid samples");
+									PressEnter();
+									Console.ReadKey();
 
 								Writeline(PartThree[13], Color.Yellow);
 
-								PressEnter();
-								Console.ReadKey();
-								EndGame();
+									PressEnter();
+									Console.ReadKey();
+
+								//Calling the congratz class
+								Congratz();
 
 							}
 
@@ -392,8 +410,12 @@ namespace Space_Disasters
 
 								Writeline(PartThree[8], Color.Yellow);
 
-								PressEnter();
-								Console.ReadKey();
+									PressEnter();
+									Console.ReadKey();
+								
+								Inventory.Clear();
+								
+								//Calling the endgame class
 								EndGame();
 
 
@@ -403,49 +425,53 @@ namespace Space_Disasters
 						Write(PartThree[10], Color.Yellow);
 
 							EnterYourChoice();
-						AnswerInput();
+							AnswerInput();
 
 						if (input == "yes")
 						{
 
 							Writeline(PartThree[11] + PartThree[12], Color.Yellow);
 
-							PressEnter();
-							Console.ReadKey();
+							Inventory.Add("Spacecraft Keys" + "Remainings of iron, copper and silicon" + "Astroid samples");
+								
+								PressEnter();
+								Console.ReadKey();
 							
+							
+
 							Writeline(PartThree[13], Color.Yellow);
 							
-							PressEnter();
-							Console.ReadKey();
-							EndGame();
+								PressEnter();
+								Console.ReadKey();
+
+							//Calling the congratz class
+							Congratz();
 						}
 						else
 							Write(PartThree[8], Color.Yellow);
 
 
-						Writeline(PartThree[11], Color.Yellow);
-						Writeline(PartThree[12], Color.Yellow);
+								PressEnter();
+								Console.ReadKey();
+							Inventory.Clear();
 
-						Console.Clear();
-
-						Writeline(PartThree[13], Color.Yellow);
-
+							//Calling the endgame class
+							EndGame();
 
 						Writeline("", Color.White);
-
 						PressEnter();
 
 						break;
 					default:
-						//if scenario does not match any above
+						EndGame();
 						break;
 				}
 		}
 }
 
-		static int Scenarios = 3;
+		private static int Scenarios = 3;
 
-		static string[] PartOne = {
+		private static string[] PartOne = {
 
 			"You have been launched into space for a mission. The launch seems to be going well until, it left the atmosphere of the Earth. \n\nThen problems start to occur slowly!\n",
 			"\nThe third stage of the rocket crashes into the space station before it could slow down for docking. ",
@@ -470,22 +496,22 @@ namespace Space_Disasters
 
 		};
 
-		static string[] PartTwo = {
+		private static string[] PartTwo = {
 
 			"\nYou go back and open the first drawer and find some batteries. After that you keep looking for another way out. ",
 			"You find a safe, \nit is locked but can be unlocked with a special code. You need to look for the code! You see a mirror hanging on the wall, turn the mirror around?\n",
 			//No
 			"\nThe air pressure gets so low that you can't breath anymore which unfortunatly cause you a horrible death!\n",
 			//Yes
-			"\nYou see a code that can be used to open the safe. You open the safe with the code seen on the back side of the mirror. You find a key to open the door of the room as well as a flashlight without batteries, pretty unfortunate huh? \nYou proceed with opening the door with the provided key and you put the previous found batteries in the flashlight. You turn on the flashlight, now you need to get out of the rocket \nseen that the air pressure is getting low and you don't time. You get out of the first room and see a blocked door, you also see a window by shining the flashlight around. \nThe window is closed but can be opened so you attempt to open it, you open it and manage to get out of the rocket to the space station safe and sound!",
+			"\nYou see a code that can be used to open the safe. You open the safe with the code seen on the back side of the mirror. You find a key to open the door of the room as well as a flashlight without batteries, pretty unfortunate huh? \nYou proceed with opening the door with the provided key and you put the previous found batteries in the flashlight. You turn on the flashlight, now you need to get out of the rocket \nseen that the air pressure is getting low and you don't much time. You get out of the first room and see a blocked door, you also see a window by shining the flashlight around. \nThe window is closed but can be opened so you attempt to open it, you open it and manage to get out of the rocket to the space station safe and sound!",
 			"\nYou meet with an ISS crew member which has thankfully survived alongside to another member which has barely made it.",
 		};
 
-		static string[] PartThree = {
+		private static string[] PartThree = {
 
 			"\nThey provide you with some equipment and food so you can survive in space. You notice that the space station is in a bad shape and needs some repairs. \nDo you want to try to help repairing it?",
 			//First Yes
-			"\nYou look around and see some tools and some spare parts laying around in the station, they have been reserved for such situations. You put on a space suit and tie yourself to the station with \na special belt that would help you not getting lost in space. You get out of the space station and you take tools and spares with you, you float in space next to the station looking for the damages caused. \nA crew member gets out to help you fix the issues. You find a hole in the space station which is causing a leak in the oxygen that is stored. \nThe oxygen generator has also been effected by the crash which caused it to fail. Do you want to proceed with fixing the hole and the leak?\n",
+			"\nYou look around and see some tools and some spare parts laying around in the station, they have been reserved for such situations. You put on a space suit and tie yourself to the station with \na special belt that would help you not getting lost in space. You get out of the space station and you take tools and spares with you, you float in space next to the station looking for the damages caused. \nA crew member gets out to help you fix the issues. You find a hole in the space station which is causing a leak in the oxygen that is stored. \nThe oxygen generator has also been badly effected by the crash which caused it to fail. Do you want to proceed with fixing the hole and the leak?\n",
 			//First No
 			"You inform the crew that you can not help for some reason, but you will however use some spare parts to build a small rocket to get to the moon base.\n\n",
 			"You get to a special place where a small rocket can be built and attached to the station. You will need some materials for your rocket, so you go into the station and grab some spare parts. \nWhile picking up spare parts you notice that the station lacks some materials like metal, iron, copper and silicon. \n\nIn a distance you see a small space shuttle that can be used for gathering resources around from asteroids. Do you want to board it? \n",
@@ -518,7 +544,7 @@ namespace Space_Disasters
 
 
 		//A new class of the items
-		static class Items
+		private static class Items
 		{
 
 
@@ -529,11 +555,66 @@ namespace Space_Disasters
 
 
 
-
-		static void EndGame()
+		private static void Congratz()
 		{
 			Console.Clear();
 
+			string YouwinTitle = (@"
+                ______                                       _            _                  _ 
+               / _____)                            _        | |      _   (_)                | |
+              | /      ___  ____   ____  ____ ____| |_ _   _| | ____| |_  _  ___  ____   ___| |
+              | |     / _ \|  _ \ / _  |/ ___) _  |  _) | | | |/ _  |  _)| |/ _ \|  _ \ /___)_|
+              | \____| |_| | | | ( ( | | |  ( ( | | |_| |_| | ( ( | | |__| | |_| | | | |___ |_ 
+               \______)___/|_| |_|\_|| |_|   \_||_|\___)____|_|\_||_|\___)_|\___/|_| |_(___/|_|
+                                 (_____|                                                       
+");
+			Writeline(YouwinTitle, Color.Green);
+
+			Writeline("You've made it to the base safe and sound!", Color.Green);
+
+			Writeline(CharacterName + " you have collected the following throughout your journey:\n\n", Color.Purple);
+			
+			//This will list the items in inventory
+			foreach (string item in Inventory)
+				Writeline(item + Environment.NewLine, Color.White);
+
+
+			if (Inventory.Contains("Astroid samples"))
+			{
+
+				Writeline("\nGood job! You have collected the samples and returned them to the earth command base, now scientists can study them for future missions!", Color.AliceBlue);
+			}
+			else
+				Writeline("You haven't gotten the smalples, scientists are big mad!", Color.MediumVioletRed);
+
+
+			//Thnking the player ofr playing and asks if he/she wants to play again!
+			ThanksMyDude();
+			AnswerInput();
+
+			//Resturn back after loosing or finishing the game
+			if (input == "yes")
+			{
+
+				Console.ResetColor();
+				Console.Clear();
+				StartGame();
+			}
+			else
+				Environment.Exit(0);
+		}
+
+
+
+
+
+
+
+		private static void EndGame()
+		{
+			Console.Clear();
+
+			//Game over text
 			string gameover = (@"
 
                                      ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗ ██╗
